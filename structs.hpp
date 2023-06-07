aa7e23ecced4cb734144f2f8821a0ab7689ba9c7
Myp3a <murza8965@gmail.com>
2023-06-07 13:32:15 +0300

    cleanup; fan curve plots

--- structs.hpp
+++ structs.hpp
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 struct Point {
 	int temp;
 	int speed;
 };
-struct FanCurve {
+
+class FanCurve {
+public:
 	Point point[6];
+	int get_interpolated(int temp);
+};
+
+enum FAN_STATE {
+	COOLERBOOST,
+	SILENT,
+	AUTO,
+	CUSTOM
+};
+
+enum STATE {
+	ENABLED,
+	DISABLED
+};
+
+static const std::map <std::string, FAN_STATE> fan_mapping = {
+	{"coolerboost", FAN_STATE::COOLERBOOST},
+	{"silent",		FAN_STATE::SILENT},
+	{"auto",		FAN_STATE::AUTO},
+	{"custom",		FAN_STATE::CUSTOM}
+};
+
+static const std::map <std::string, STATE> state = {
+	{"enabled", STATE::ENABLED},
+	{"disabled", STATE::DISABLED}
 };
--- wyvern.cpp
+++ wyvern.cpp
@@ -2,9 +2,57 @@
 #include <Windows.h>
 
 #include "control.hpp"
+#include "structs.hpp"
+#include "ascii/asciiplotter.h"
 #include "CLI11.hpp"
 
 using namespace std;
+
+void print_system_info(Control ctrl) {
+    cout << "Wyvern v. master\n";
+    cout << "System firmware: " << ctrl.firmware_name() << '\n';
+    cout << "EC dump:\n";
+    ctrl.dump_ec();
+}
+
+void print_fan_info(Control ctrl, bool pretty = false) {
+    if (pretty) {
+        cout << "Current mode: ";
+    }
+    if (ctrl.coolerboost_get()) {
+        cout << "coolerboost\n";
+    }
+    else if (ctrl.silent_get()) {
+        cout << "silent\n";
+    }
+    else if (!ctrl.custom_curve_get()) {
+        cout << "auto\n";
+    }
+    else {
+        cout << "custom\n";
+        if (pretty) {
+            FanCurve cur_cpu = ctrl.cpu_fan_curve_get();
+            FanCurve cur_gpu = ctrl.gpu_fan_curve_get();
+            vector<double> cpu_temp;
+            vector<double> cpu_speed;
+            vector<double> gpu_temp;
+            vector<double> gpu_speed;
+            for (int i = 0; i < 6; i++) {
+                cpu_temp.push_back(cur_cpu.point[i].temp);
+                cpu_speed.push_back(cur_cpu.point[i].speed);
+                gpu_temp.push_back(cur_gpu.point[i].temp);
+                gpu_speed.push_back(cur_gpu.point[i].speed);
+            }
+            AsciiPlotter plotter("Fan speeds", 40, 15);
+            plotter.addPlot(vector<double>{100}, vector<double>{150}, " ", ' ');
+            plotter.addPlot(cpu_temp, cpu_speed, "CPU", 'c');
+            plotter.addPlot(gpu_temp, gpu_speed, "GPU", 'g');
+            plotter.legend();
+            plotter.xlabel("Deg (C)");
+            plotter.show();
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -14,238 +62,148 @@ int main(int argc, char** argv)
 
     CLI::App* version_app = app.add_subcommand("version", "Information about system and program");
 
-    CLI::App* fan_app = app.add_subcommand("fan", "Fan curve control");
-    CLI::App* coolerboost_app = fan_app->add_subcommand("coolerboost", "CoolerBoost management");
-    int coolerboost_action{ 0 };
-    coolerboost_app->add_flag("--enable{1},--disable{2}", coolerboost_action, "CoolerBoost state");
-    CLI::App* silent_app = fan_app->add_subcommand("silent", "Silent mode control");
-    int silent_action{ 0 };
-    silent_app->add_flag("--enable{1},--disable{2}", silent_action, "Silent mode state");
-    CLI::App* custom_curve_app = fan_app->add_subcommand("custom", "Custom fan curve state control");
-    int custom_curve_action{ 0 };
-    custom_curve_app->add_flag("--enable{1},--disable{2}", custom_curve_action, "Custom fan curve state");
-    fan_app->require_subcommand(0, 1);
-    string curve_text = "";
-    bool gpu{ false };
-    bool cpu{ false };
-    vector<pair<int,int>> points;
-    fan_app->add_flag("--gpu", gpu, "Only GPU fan");
-    fan_app->add_flag("--cpu", cpu, "Only CPU fan");
-    CLI::Option* fan_set = fan_app->add_flag("--set", "Set new curve");
-    fan_app->add_option("-p,--point", points, "Point on the curve in format: [temp,speed]. Max 6 points.")->expected(0, 7)->needs(fan_set);
-    
-    CLI::App* battery_app = app.add_subcommand("battery", "Battery threshold control");
-    int batt_threshold{ -1 };
-    battery_app->add_option("--percent", batt_threshold, "Battery charge limit");
+    CLI::App* fan_app = app.add_subcommand("fan", "Fan management");
+    CLI::Option* fan_set = fan_app->add_flag("--set", "Set fans mode");
+    string desired_fan_mode;
+    fan_app->add_option("fan_mode", desired_fan_mode, "Selected fan mode")->check(CLI::IsMember({ "coolerboost", "silent", "auto", "custom" }));
+    vector<pair<int, int>> desired_fan_curve;
+    CLI::Option* fan_curve_points = fan_app->add_option("-p,--point", desired_fan_curve, "Fan curve points in format [temp,speed %]")->expected(0, 7)->needs(fan_set);
+    bool ignore_cpu{ false };
+    bool ignore_gpu{ false };
+    fan_app->add_flag("--no-cpu", ignore_cpu, "Disables CPU curve setting");
+    fan_app->add_flag("--no-gpu", ignore_gpu, "Disables GPU curve setting");
 
-    CLI::App* webcam_app = app.add_subcommand("webcam", "Webcam control");
-    int webcam_selection{ 0 };
-    webcam_app->add_flag("--first{1},--second{2}", webcam_selection, "Webcam lock selection");
-    CLI::Option* enable_webcam = webcam_app->add_flag("--enable", "Enable webcam");
-    CLI::Option* disable_webcam = webcam_app->add_flag("--disable", "Disable webcam");
-    enable_webcam->excludes(disable_webcam);
-    disable_webcam->excludes(enable_webcam);
+    CLI::App* webcam_app = app.add_subcommand("webcam", "Webcam lock management");
+    string desired_webcam_state;
+    webcam_app->add_option("state", desired_webcam_state, "Webcam state")->check(CLI::IsMember({ "enabled", "disabled" }));
+    int webcam_first_second_lock{ 0 };
+    webcam_app->add_flag("-1{1},-2{2}", webcam_first_second_lock, "Lock selection");
 
+    CLI::App* battery_app = app.add_subcommand("battery", "Battery charging limit");
+    int battery_percent{ 0 };
+    battery_app->add_option("percent", battery_percent, "Percent to end charge at")->check(CLI::Range(10,100));
 
     CLI11_PARSE(app, argc, argv);
     Control ctrl = Control();
 
     if (*version_app) {
-        cout << "Wyvern v. master\n";
-        cout << "System firmware: " << ctrl.firmware_name() << '\n';
-        cout << "EC dump:\n";
-        ctrl.dump_ec();
+        print_system_info(ctrl);
         return 0;
     }
 
     if (*fan_app) {
-        if (!(gpu) && !(cpu)) {
-            gpu = true;
-            cpu = true;
-        }
-        if (*coolerboost_app) {
-            string s;
-            switch (coolerboost_action) {
-            case 0:
-                s = "CoolerBoost state: ";
-                if (ctrl.coolerboost_get()) {
-                    s += "enabled\n";
-                }
-                else {
-                    s += "disabled\n";
-                }
-                cout << s;
-                break;
-            case 1:
-                ctrl.coolerboost_set(true);
-                break;
-            case 2:
-                ctrl.coolerboost_set(false);
-                break;
-            }
-            return 0;
-        }
-        if (*silent_app) {
-            string s;
-            switch (silent_action) {
-            case 0:
-                s = "Silent mode state: ";
-                if (ctrl.silent_get()) {
-                    s += "enabled\n";
-                }
-                else {
-                    s += "disabled\n";
-                }
-                cout << s;
-                break;
-            case 1:
-                ctrl.silent_set(true);
-                break;
-            case 2:
-                ctrl.silent_set(false);
-                break;
-            }
-            return 0;
-        }
-        if (*custom_curve_app) {
-            string s;
-            switch (custom_curve_action) {
-            case 0:
-                s = "Custom curve mode state: ";
-                if (ctrl.custom_curve_get()) {
-                    s += "enabled\n";
-                }
-                else {
-                    s += "disabled\n";
-                }
-                cout << s;
-                break;
-            case 1:
-                ctrl.custom_curve_set(true);
-                break;
-            case 2:
-                ctrl.custom_curve_set(false);
-                break;
-            }
-            return 0;
-        }
-        if (*fan_set) {
+        if (!desired_fan_curve.empty()) {
             FanCurve cur = ctrl.cpu_fan_curve_get();
             int i = 0;
-            for (; i < points.size(); i++) {
-                cur.point[i].temp = points[i].first;
-                cur.point[i].speed = points[i].second;
+            // fill with user input
+            for (; i < desired_fan_curve.size(); i++) {
+                cur.point[i].temp = desired_fan_curve[i].first;
+                cur.point[i].speed = desired_fan_curve[i].second;
             }
+            // finish with repeating last one
             for (; i < 6; i++) {
-                cur.point[i].temp = points.back().first;
-                cur.point[i].speed = points.back().second;
+                cur.point[i].temp = desired_fan_curve.back().first;
+                cur.point[i].speed = desired_fan_curve.back().second;
             }
-            if (gpu) {
+            if (!ignore_gpu) {
                 ctrl.gpu_fan_curve_set(cur);
             }
-            if (cpu) {
+            if (!ignore_cpu) {
                 ctrl.cpu_fan_curve_set(cur);
+            }
+        }
+        if (!desired_fan_mode.empty()) {
+            if (*fan_set) {
+                switch (fan_mapping.at(desired_fan_mode)) {
+                case FAN_STATE::COOLERBOOST:
+                    ctrl.coolerboost_set(true);
+                    ctrl.silent_set(false);
+                    ctrl.custom_curve_set(false);
+                    break;
+                case FAN_STATE::SILENT:
+                    ctrl.coolerboost_set(false);
+                    ctrl.silent_set(true);
+                    ctrl.custom_curve_set(false);
+                    break;
+                case FAN_STATE::AUTO:
+                    ctrl.coolerboost_set(false);
+                    ctrl.silent_set(false);
+                    ctrl.custom_curve_set(false);
+                    break;
+                case FAN_STATE::CUSTOM:
+                    ctrl.coolerboost_set(false);
+                    ctrl.silent_set(false);
+                    ctrl.custom_curve_set(true);
+                    break;
+                }
+                return 0;
+            }
+            else {
+                switch (fan_mapping.at(desired_fan_mode)) {
+                case FAN_STATE::COOLERBOOST:
+                    cout << ctrl.coolerboost_get();
+                    break;
+                case FAN_STATE::SILENT:
+                    cout << ctrl.silent_get();
+                    break;
+                case FAN_STATE::AUTO:
+                    cout << !ctrl.custom_curve_get();
+                    break;
+                case FAN_STATE::CUSTOM:
+                    cout << ctrl.custom_curve_get();
+                    break;
+                }
+                return 0;
+            }
+        }
+        print_fan_info(ctrl, true);
+        return 0;
+    }
+
+    if (*webcam_app) {
+        if (!desired_webcam_state.empty()) {
+            switch (state.at(desired_webcam_state)) {
+            case STATE::ENABLED:
+                if (webcam_first_second_lock == 0 || webcam_first_second_lock == 1) {
+                    ctrl.webcam_first_set(true);
+                }
+                if (webcam_first_second_lock == 0 || webcam_first_second_lock == 2) {
+                    ctrl.webcam_second_set(true);
+                }
+                break;
+            case STATE::DISABLED:
+                if (webcam_first_second_lock == 0 || webcam_first_second_lock == 1) {
+                    ctrl.webcam_first_set(false);
+                }
+                if (webcam_first_second_lock == 0 || webcam_first_second_lock == 2) {
+                    ctrl.webcam_second_set(false);
+                }
+                break;
             }
             return 0;
         }
-        cout << "First enabled from top to bottom will take priority:\n";
-        string s = "CoolerBoost state: ";
-        if (ctrl.coolerboost_get()) {
-            s += "enabled\n";
-        }
-        else {
-            s += "disabled\n";
-        }
-        cout << s;
-        s = "Silent mode state: ";
-        if (ctrl.silent_get()) {
-            s += "enabled\n";
-        }
-        else {
-            s += "disabled\n";
-        }
-        cout << s;
-        s = "Custom curve mode state: ";
-        if (ctrl.custom_curve_get()) {
-            s += "enabled\n";
-        }
-        else {
-            s += "disabled\n";
-        }
-        cout << s;
-        cout << '\n';
-        FanCurve cur_cpu = ctrl.cpu_fan_curve_get();
-        FanCurve cur_gpu = ctrl.gpu_fan_curve_get();
-        if (cpu) {
-            cout << "CPU Fan speed: " << ctrl.cpu_fan_rpm() << " RPM (" << ctrl.cpu_fan_percent() << "%)\n";
-            cout << "CPU fan curve:\n";
-            for (int i = 0; i < 6; i++) {
-                cout << cur_cpu.point[i].temp << " C: " << cur_cpu.point[i].speed << " %\n";
-            }
-            cout << "\n";
-        }
-        if (gpu) {
-            cout << "GPU Fan speed: " << ctrl.gpu_fan_rpm() << " RPM (" << ctrl.gpu_fan_percent() << "%)\n";
-            cout << "GPU fan curve:\n";
-            for (int i = 0; i < 6; i++) {
-                cout << cur_gpu.point[i].temp << " C: " << cur_gpu.point[i].speed << " %\n";
-            }
-            cout << "\n";
+        switch (webcam_first_second_lock) {
+        case 0:
+            cout << "First webcam state: " << ctrl.webcam_first_get() << "\n";
+            cout << "Second webcam state: " << ctrl.webcam_second_get() << "\n";
+            break;
+        case 1:
+            cout << ctrl.webcam_first_get() << "\n";
+            break;
+        case 2:
+            cout << ctrl.webcam_second_get() << "\n";
+            break;
         }
         return 0;
     }
 
     if (*battery_app) {
-        if (batt_threshold > -1) {
-            if (batt_threshold < 10 || batt_threshold > 100) {
-                cout << "Battery threshold should be between 10 and 100\n";
-                return 1;
-            }
-            ctrl.battery_threshold_set(batt_threshold);
-            return 0;
+        if (battery_percent > 0) {
+            ctrl.battery_threshold_set(battery_percent);
         }
-        cout << "Battery charge threshold: " << ctrl.battery_threshold_get() << "%\n";
+        else {
+            cout << ctrl.battery_threshold_get() << "\n";
+        }
         return 0;
     }
-
-    if (*webcam_app) {
-        if (*enable_webcam) {
-            if (webcam_selection == 0 || webcam_selection == 1) {
-                ctrl.webcam_first_set(true);
-            }
-            if (webcam_selection == 0 || webcam_selection == 2) {
-                ctrl.webcam_second_set(true);
-            }
-            return 0;
-        }
-        if (*disable_webcam) {
-            if (webcam_selection == 0 || webcam_selection == 1) {
-                ctrl.webcam_first_set(false);
-            }
-            if (webcam_selection == 0 || webcam_selection == 2) {
-                ctrl.webcam_second_set(false);
-            }
-            return 0;
-        }
-        if (webcam_selection == 0 || webcam_selection == 1) {
-            cout << "First webcam switch enabled: ";
-            if (ctrl.webcam_first_get()) {
-                cout << "True\n";
-            }
-            else {
-                cout << "False\n";
-            }
-        }
-        if (webcam_selection == 0 || webcam_selection == 2) {
-            cout << "Second webcam switch enabled: ";
-            if (ctrl.webcam_second_get()) {
-                cout << "True\n";
-            }
-            else {
-                cout << "False\n";
-            }
-        }
-    }
-
-    return 0;
 }
